2afa0afb005478befa44774edf8ba09ca2fca01a
nitin reddy <reddynitin3@gmail.com>
2017-02-11 13:45:05 +0530

    JSON data serving

--- examples/app-server.js
+++ examples/app-server.js
@@ -3,10 +3,15 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     console.log('Request URL: ' + req.url);
-    res.writeHead('200', { 'Content-Type': 'text/html'});
+    res.writeHead('200', { 'Content-Type': 'application/json'});
     // var readStream = fs.createReadStream(__dirname + '/test.txt');
-    var readStream = fs.createReadStream(__dirname + '/head.html', 'utf8');
-    readStream.pipe(res);
+    // var readStream = fs.createReadStream(__dirname + '/head.html', 'utf8');
+    var myObj = {
+        name: 'Nitin',
+        age: 28
+    }
+    // readStream.pipe(res);
+    res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
