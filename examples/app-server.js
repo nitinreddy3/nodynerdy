8c557225f63f494d742e44eef1220387050cb4bb
nitin reddy <reddynitin3@gmail.com>
2017-02-11 10:20:21 +0530

    Example on html files from server to client

--- examples/app-server.js
+++ examples/app-server.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     console.log('Request URL: ' + req.url);
-    res.writeHead('200', { 'Content-Type': 'text/plain'});
-    res.end('Hey bro');
+    res.writeHead('200', { 'Content-Type': 'text/html'});
+    // var readStream = fs.createReadStream(__dirname + '/test.txt');
+    var readStream = fs.createReadStream(__dirname + '/head.html', 'utf8');
+    readStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
