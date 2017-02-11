e56211f4a66e448a52eca174f55de0f8951723a3
nitin reddy <reddynitin3@gmail.com>
2017-02-11 23:15:41 +0530

    Dynamic data rendering

--- examples/app-express.js
+++ examples/app-express.js
@@ -2,16 +2,18 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('This is a home page')
+    res.sendFile(__dirname + '/index.html')
 });
 
 app.get('/contact', (req, res) => {
-    res.send('This is a contact page')
+    res.sendFile(__dirname + '/contact.html')
 });
 
-app.get('/profile/:id', (req, res) => {
-    res.send('This is a profile with id: ' + req.params.id);
+app.get('/profile/:name', (req, res) => {
+    res.render('profile', { person: req.params.name });
 });
 
 app.listen(3000);
